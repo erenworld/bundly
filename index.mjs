7f67202ce8ec17f562104c1f26382427ef9e4fae
eren <eren.turkoglu@epitech.eu>
2025-08-29 18:15:03 +0200

    Add search for tests files on the file system

---
+++ index.mjs
@@ -0,0 +1,29 @@
+import { glob } from "glob";
+import JestHasteMap from "jest-haste-map";
+import { cpus, platform } from "os";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+// const testFiles = glob.sync("**/*.test.js");
+// console.log(testFiles); // ['tests/01.test.js', 'tests/02.test.js', …]
+
+const root = dirname(fileURLToPath(import.meta.url));
+const hasteMapOptions = {
+  extensions: ["js"],
+  maxWorkers: cpus().length,
+  name: "bundly",
+  platforms: [],
+  rootDir: root,
+  roots: [root],
+};
+
+const hasteMap = new JestHasteMap.default(hasteMapOptions);
+await hasteMap.setupCachePath(hasteMapOptions);
+
+// Build and return an in-memory HasteFS ("Haste File System") instance.
+const { hasteFS } = await hasteMap.build();
+
+const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js']);
+
+// ['/path/to/tests/01.test.js', '/path/to/tests/02.test.js', …]
+console.log(testFiles);
