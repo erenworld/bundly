2324f8e4bef4cad0e5f8f0b714f6be97da2b3c92
eren <eren.turkoglu@epitech.eu>
2025-08-30 21:17:07 +0200

    Add prettier

---
+++ index.js
@@ -0,0 +1,58 @@
+import HasteMap from 'jest-haste-map';
+import { cpus } from 'os';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
+import chalk from 'chalk';
+import yargs from 'yargs';
+import Resolver from 'jest-resolve';
+import { DependencyResolver } from 'jest-resolve-dependencies';
+
+const root = join(dirname(fileURLToPath(import.meta.url)), 'product');
+
+const hasteMapOptions = {
+  extensions: ['js'],
+  maxWorkers: cpus().length,
+  name: 'jest-bundler',
+  platforms: [],
+  rootDir: root,
+  roots: [root],
+};
+
+const map = new HasteMap(hasteMapOptions);
+await map.setupCachePath(hasteMapOptions);
+const { hasteFS, moduleMap } = await map.build();
+console.log(hasteFS.getAllFiles());
+// ['/path/to/product/apple.js', '/path/to/product/banana.js', …]
+
+const options = yargs(process.argv).argv;
+const entryPoint = resolve(process.cwd(), options.entryPoint);
+if (!hasteFS.exists(entryPoint)) {
+  throw new Error(
+    '`--entry-point` does not exist. Please provide a path to a valid file.',
+  );
+}
+
+const resolver = new Resolver(moduleMap, {
+  extensions: ['.js'],
+  hasCoreModules: false,
+  rootDir: root,
+});
+const dependencyResolver = new DependencyResolver(resolver, hasteFS);
+
+const allFiles = new Set();
+const queue = [entryPoint];
+
+while (queue.length) {
+  const module = queue.shift();
+
+  if (allFiles.has(module)) {
+    continue;
+  }
+  allFiles.add(module);
+  queue.push(...dependencyResolver.resolve(module));
+}
+
+console.log(chalk.bold(`❯ Found ${chalk.blue(allFiles.size)} files`));
+console.log(Array.from(allFiles));
+// node index.mjs --entry-point product/entry-point.js
